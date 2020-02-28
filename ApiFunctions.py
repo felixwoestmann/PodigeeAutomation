9286feb43d00fdbda3b917ab2a58ca421d9963f3
Felix Wöstmann <f.wosti@gmail.com>
2020-02-28 11:06:39 +0100

    Obtain Number for Podcast is working properly

--- ApiFunctions.py
+++ ApiFunctions.py
@@ -10,18 +10,19 @@ def load_apitoken():
         return data["token"]
 
 
+def create_podigee_header():
+    return {"Token": load_apitoken(), "Accept": "application / json", "Content-Type": "application/json"}
+
+
 # Pull Number of Last Episode
-def get_latest_episodenumber(podcast_id):
-    headers = {"Token": load_apitoken(), "Accept": "application / json", "Content-Type": "application/json"}
+def get_latest_episode_number(podcast_id):
     payload = {"published": True, "limit": 1, "podcast_id": podcast_id, "sort_by": "number",
                "sort_direction": "desc"}
-    request = requests.get(episode_url, headers=headers, data=json.dumps(payload))
-    if request.status_code == 200:
-        return request.json()['number']
-    else:
-        return -1
+    request = requests.get(episode_url, headers=create_podigee_header(), data=json.dumps(payload))
+    return request.json()[0]['number']
 
 
-def create_podcast_episode(headers, payload):
-    request = requests.post(episode_url, headers=headers, data=json.dumps(payload))
+
+def create_podcast_episode(payload):
+    request = requests.post(episode_url, headers=create_podigee_header(), data=json.dumps(payload))
     print(request.status_code)
